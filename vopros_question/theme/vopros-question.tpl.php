58f5048a5c55d053436251b0e8a33094e69fde8a
Jakob Torp <jakob@revealit.dk>
2011-05-18 18:47:38 +0200

    Updated question view page with form actions.

--- vopros_question/theme/vopros-question.tpl.php
+++ vopros_question/theme/vopros-question.tpl.php
@@ -2,3 +2,20 @@
 /**
  *
  */
+?>
+<div id="question-<?php print $question->question_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  <div class="content clearfix"<?php print $content_attributes; ?>>
+    <?php
+      if (isset($content['action'])) {
+        hide($content['action']);
+      }
+      print render($content);
+    ?>
+  </div>
+  <?php if (isset($content['action'])): ?>
+    <div class="action">
+      <?php print render($content['action']); ?>
+    </div>
+  <?php endif ?>
+
+</div>
