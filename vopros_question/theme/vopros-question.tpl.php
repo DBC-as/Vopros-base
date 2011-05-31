8b139298f104e2b31b497c1693957a1ce3b44114
Jakob Torp <jakob@revealit.dk>
2011-05-31 13:27:19 +0200

    Added mail correspondence to question view.

--- vopros_question/theme/vopros-question.tpl.php
+++ vopros_question/theme/vopros-question.tpl.php
@@ -6,16 +6,12 @@
 <div id="question-<?php print $question->question_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <div class="content"<?php print $content_attributes; ?>>
     <?php
-      if (isset($content['action'])) {
-        hide($content['action']);
-      }
       print render($content);
     ?>
   </div>
-  <?php if (isset($content['action'])): ?>
-    <div class="action">
-      <?php print render($content['action']); ?>
+  <?php if (isset($right) && !empty($right)): ?>
+    <div class="action right">
+      <?php print render($right); ?>
     </div>
   <?php endif ?>
-
 </div>
